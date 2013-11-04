9371e5e4c15c0477838f7c062cb29e6458441149
Erika <moriceerika@gmail.com>
2013-11-04 11:39:07 +0100

    Binding on user's profile (connection)

---
+++ morice_eMorbius/Wpf-Morbius/Tools/BoolColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Wpf_Morbius
+{
+    class BoolColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                bool tmpValue = System.Convert.ToBoolean(value);
+
+                if (tmpValue)
+                    return Brushes.Green;
+            }
+            return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ morice_eMorbius/Wpf-Morbius/Tools/BoolConnectedConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Wpf_Morbius
+{
+    class BoolConnectedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                bool tmpValue = System.Convert.ToBoolean(value);
+
+                if (tmpValue)
+                    return "Connecté";
+            }
+            return "Non Connecté";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
