5d19dddb1ddd78e02bc5d040baeddf18e2982218
Erika <moriceerika@gmail.com>
2013-11-04 19:22:12 +0100

    Observation
    with binding (exept : the add button)
    without (every) styles

--- morice_eMorbius/Wpf-Morbius/ViewModel/PatientObsViewModel.cs
+++ morice_eMorbius/Wpf-Morbius/ViewModel/PatientObsViewModel.cs
@@ -8,9 +8,17 @@ namespace Wpf_Morbius.ViewModel
 {
     class PatientObsViewModel : BaseViewModel
     {
+        private ServicePatient.Observation[] _obs;
+
+        public ServicePatient.Observation[] Obs
+        {
+            get { return _obs; }
+            set { _obs = value; }
+        }
+
         public PatientObsViewModel()
         {
-            
+            _obs = PatientViewModel.Patient.Observations;
         }
     }
 }
