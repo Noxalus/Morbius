5c9b94aec9f729e2b48596d8f9de63dfe957e489
Erika <moriceerika@gmail.com>
2013-11-14 17:32:36 +0100

    AddObservationWindow

--- morice_eMorbius/Wpf-Morbius/ViewModel/PatientObsViewModel.cs
+++ morice_eMorbius/Wpf-Morbius/ViewModel/PatientObsViewModel.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Wpf_Morbius.ViewModel
 {
     class PatientObsViewModel : BaseViewModel
     {
         private ServicePatient.Observation[] _obs;
+        private ICommand _addCommand;
+
+        public ICommand AddCommand
+        {
+            get { return _addCommand; }
+            set { _addCommand = value; }
+        }
 
         public ServicePatient.Observation[] Obs
         {
@@ -19,6 +27,22 @@ namespace Wpf_Morbius.ViewModel
         public PatientObsViewModel()
         {
             _obs = PatientViewModel.Patient.Observations;
+
+            // Commandes
+            _addCommand = new RelayCommand(param => addObs(), param => true);
         }
+
+        /// <summary>
+        /// action permettant d'ouvrir la pop-up
+        /// </summary>
+        private void addObs()
+        {
+            var window = new View.PatientObsAdd();
+            //var vm = new PatientObsAddViewModel();
+            //window.DataContext = vm;
+            window.Show();      
+        }
+
+
     }
 }
